a25724793d64b074fc8411710c344fed5d03f288
Orlando Mendoza <ommv.17@gmail.com>
2025-06-17 11:31:54 -0400

    Prueba de referer

--- src/app/api/pruebaReferer/route.ts
+++ src/app/api/pruebaReferer/route.ts
@@ -3,7 +3,10 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(request: NextRequest) {
   // Obtener el referer de los headers de la petición
   // Devolver el referer en la respuesta
+  console.log('request', request)
   return NextResponse.json({
-    request
+    hostDomain: request.headers.get('host'),
+    originDomain: request.headers.get('origin'),
+    refererDomain: request.headers.get('referer'),
   });
 }
